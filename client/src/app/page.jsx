84fc30c7286286e0446c9646cdc700c13c2d35f0
Binhkio <quan.va205115@gmail.com>
2024-06-06 13:19:20 +0700

    update

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main className="">
-      Home
+    <main className="flex justify-center align-top gap-x-10">
+      <div>
+        <div>Popular tags</div>
+        <div>{['oop','lythuyet','web','os','network'].map((val) => (
+          <div>
+            
+          </div>
+        ))}</div>
+      </div>
     </main>
   );
 }
