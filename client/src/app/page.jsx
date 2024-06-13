92bd17b52f261e88b0061ecc2dc4d174c725413b
Binhkio <quan.va205115@gmail.com>
2024-06-13 10:57:06 +0700

    update

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -3,14 +3,11 @@ import { Button, Flex } from 'antd';
 import QuestionForm from '@/components/QuestionForm';
 
 export default function Home() {
-  const createForm = () => {
-    QuestionForm();
-  }
   return (
     <div style={{maxWidth: "600px", margin: "auto", paddingTop: "30px"}}>
       <div style={{"display": "flex", justifyContent: "space-between"}}>
         <div style={{fontWeight: "400", fontSize: "32px", lineHeight: "40px"}}>Các Câu Hỏi</div>
-        <Button onClick={createForm} type='primary' size='large'>Tạo Câu Hỏi</Button>
+        <QuestionForm/>
       </div>
       <div className='mt-8 flex flex-col gap-4'>
         <QuestionCard/>
