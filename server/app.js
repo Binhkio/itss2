5f43fddf47ba37cf79e9d7bc49768b69ddd09b72
Binhkio <101411388+Binhkio@users.noreply.github.com>
2024-06-14 10:57:55 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,10 +10,6 @@ const connectToDatabase = require('./database/connect');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,9 +31,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
 });
 
 module.exports = app;
